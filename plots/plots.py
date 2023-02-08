933f5a2e85a0982a0825261253b48a02db94c298
Maxim Zaharuk <director.potok@gmail.com>
2023-02-09 00:24:08 +0300

    quote_plot_creation added

--- plots/plots.py
+++ plots/plots.py
@@ -8,6 +8,39 @@ import json
 
 
 async def make_quote_plot(response, market_type, time_frame):
-    data = [[price for price in json.loads(response.text)[f"Time Series {market_type} ({time_frame})"][date]]
-            for date in json.loads(response.text)[f"Time Series {market_type} ({time_frame})"]]
+    date = []
+    open = []
+    close = []
+    high = []
+    low = []
+    for key, value in response[f"Time Series {market_type} ({time_frame})"].items():
+        date.append(key)
+        open.append(value["1. open"])
+        high.append(value["2. high"])
+        low.append(value["3. low"])
+        close.append(value["4. close"])
+    prices = await pd.DataFrame({
+        "date": date,
+        "open": open,
+        "close": close,
+        "high": high,
+        "low": low
+    })
+    ohlc = await prices.loc[:, ['date', 'open', 'high', 'low', 'close']]
+    ohlc['date'] = await pd.to_datetime(ohlc['date'])
+    ohlc['date'] = await ohlc['date'].apply(mpl_dates.date2num)
+    ohlc = await ohlc.astype(float)
+    fig, ax = plt.subplots()
+
+    candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='blue',
+                     colordown='green', alpha=0.4)
+
+    date_format = mpl_dates.DateFormatter('%d-%m-%Y')
+    await ax.xaxis.set_major_formatter(date_format)
+    await fig.autofmt_xdate()
+
+    await fig.tight_layout()
+
+    return await plt.show()
+
 
